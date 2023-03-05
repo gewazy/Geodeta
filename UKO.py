5883017e966e2bc6ca1b028360478232235ca616
gewazy <66756558+gewazy@users.noreply.github.com>
2023-03-05 14:53:07 +0100

    Update UKO.py
    cleaning

--- UKO.py
+++ UKO.py
@@ -14,15 +14,7 @@ cnxn.commit()
 cnxn.close()
 
 print("Tworzę plik UKO!")
-''''
-# Zapisanie pliku UKO, gdy jest wymiana, zakomentuj i użyj następnego kawałka kodu
-with open(f".\\output\\uko\\{str(datetime.now().strftime('%Y%m%d_%H%M'))}.uko", 'w') as file:
-    for item in uko:
-        file.write(
-            f'{item[0]}{item[1]}{13 * " "}{item[2]}{25 * " "}{round(float(item[3]), 1)} {round(float(item[4]), 1)}  {round(float(item[5]), 1)} {item[6] if len(item[6]) > 1 else str(item[6]) + "  "}   {int(item[7])}\n')
-'''
 
-# odkomentować jak będzie wymiana..
 try:
     print('Próbuję zapisać UKO na wymianie')
     with open(f"\\\\192.168.41.141\\Public\\GEODEZJA\\UKO\\{str(datetime.now().strftime('%Y%m%d_%H%M'))}.uko", 'w') as file:
