a99e4ad82a76628c89364326fcec4ecec3f3f9d2
gewazy <66756558+gewazy@users.noreply.github.com>
2023-04-25 16:28:49 +0200

    Add files via upload
    dodanie wyboru dnia z jakiego jest robiony raport

--- init.py
+++ init.py
@@ -1,18 +1,42 @@
 import pyodbc
 
-GRUPA = 'PL186'
-GEODETA = "Dominik Jurkowski"
-ZUPT_BRYG = '[Warzycki, Prządka], [Szetlak, Gubała], [Zaleski, Nizioł], [Adamski, Rygielski]\n'
 
-# ************************* PLIKI **********************************************
+def comment(a):
+    '''Zapytanie o komentarz do raportu DPR'''
+    return a + input(f"\nWprowadź komentarz: \n{a}")
+
+
+def ddiff():
+    '''Zapytanie  o dzień, z którego chemy zrobić raport'''
+
+    try:
+        a = int(input('Wpisz Daydiff:\n 0 -> dzisiejsze dane, 1-> dane wczorajsze:\n'))
+    except ValueError:
+        print('Podaj wartość liczbową 0 lub 1.')
+        ddiff()
+    else:
+        if a == 0 or a == 1:
+            return a
+        else:
+            print('Podaj wartość liczbową 0 lub 1')
+            ddiff()
+
+
+# Dane podstawowe:
+GRUPA = 'PL192'
+GEODETA = "Dominik Jurkowski"
+ZUPT_BRYG = '[Goleń, Szatkowski], [Krawczyk, Dyja], [Istal, Nizioł]\n'
+KOMENTARZ = 'Mało dróg, istniejące drogi trudne i niebezpieczne, długie przejścia. Bardzo dalekie dojazdy (ponad 1h) '
+DDIFF = ddiff()
+## ************************* PLIKI *********************************************
 
 ## pliki wejsciowe
-MDB = r"c:\PL-186_Zapalow_3D\01_database\Pl-186_Zapalow_3D.mdb"
-DRILL_RAPORT = r"c:\PL-186_Zapalow_3D\09_wiertnictwo\Raport wiertnictwa PL186.xls"
+MDB = r"c:\PL-192_Dylagowa_3D\05_database\PL-192_DYLAGOWA_3D.mdb"
+DRILL_RAPORT = r"c:\PL-192_Dylagowa_3D\09_wiertnictwo\Raport wiertnictwa PL192.xls"
 
 ## pliki wyjściowe
-DPR = (r'c:\PL-186_Zapalow_3D\08_raport_geodezyjny\PL_186_Raport_geodezyjny_DPR.xlsx',
-       r'c:\PL-186_Zapalow_3D\08_raport_geodezyjny\2022 PL186_ raport geodezja.xls')
+DPR = (r'c:\PL-192_Dylagowa_3D\06_raporty\\PL-192_Raport_geodezyjny_dzienny.xlsx',
+       r'c:\PL-192_Dylagowa_3D\06_raporty\\2023 PL-192 raport geodezja.xlsm')
 JSON_FILE = r'.\output\line_station.json'
 QC_DOMIAR_GPS = rf'.\output\{GRUPA}_QC_Domiar_GPS.csv'
 QC_DOMIAR_ZUPT = rf'.\output\{GRUPA}_QC_Domiar_ZUPT.txt'
@@ -22,11 +46,9 @@ WZNAWIANIE_FILE = rf'.\output\{GRUPA}_max_indeks.txt'
 # ***********************ZMIENNE PODSTAWOWE ************************************
 
 # zakresy linii punktów strzałowych i geofonów
-SOURCES_TRACK = "2064 And 2424"
-RECEIVERS_TRACK = "3001 And 3761"
+SOURCES_TRACK = "2210 And 2980"
+RECEIVERS_TRACK = "1001 And 1505"
 
-# w zależności kiedy robimy raport, domyślnie ddiff = 0
-DDIFF = 0
 
 # descriptory
 VIBRATORY_DSC = "('x40', 'x45', 'x30', 'x35', 'x20', 'x25', 'x10', 'x15', 'xm', 'xm5')"
@@ -52,9 +74,13 @@ else:
 # *********************** ADRESY EMAIL *****************************************
 
 SENDER = f"{GRUPA}surveyor@geofizyka.pl"
-SUPERVISOR = 'Katarzyna.Szkliniarz@pgnig.pl'
+SUPERVISOR = 'ibort@o2.pl'
 RECIPIENT_DPR = (f"{GRUPA}partychief@geofizyka.pl",
                  "Wojciech.Burczynski@geofizyka.pl", "maciej.gruza@geofizyka.pl",
-                 "tomasz.stankiewicz@geofizyka.pl", "ryszard.kolacz@geofizyka.pl",
+                 "tomasz.stankiewicz@geofizyka.pl", "ryszard.kolacz@geofizyka.pl", "renata.ciechanska@geofizyka.pl",
                  "andrzej.czemerzynski@geofizyka.pl", f"{GRUPA}geophysicist@geofizyka.pl")
-RECIP_JSON = (f"{GRUPA}transcriber@geofizyka.pl", f"{GRUPA}lineboss@geofizyka.pl")
+RECIP_JSON = (f"{GRUPA}transcriber@geofizyka.pl", f"{GRUPA}lineboss@geofizyka.pl", "Slawomir.Bloniarz@geofizyka.pl")
+
+
+if __name__ == "__main__":
+    print('Plik przechowuje podstawowe parametry')
